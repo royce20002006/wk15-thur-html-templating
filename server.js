6321719ccaa137cdd8a9d2bd48777acd8d8d6664
Royce Calvin <royce20002006@yahoo.com>
2024-03-08 19:47:07 -0600

    finished

--- server.js
+++ server.js
@@ -89,26 +89,76 @@ const server = http.createServer((req, res) => {
     // Phase 1: GET /dogs
     if (req.method === 'GET' && req.url === '/dogs') {
       // Your code here 
+
+      const dogList = fs.readFileSync('./views/dogs.html', 'utf-8')
+      let dogItems = '';
+      dogs.forEach(dog => {
+        dogItems += `<li>${dog.name}</li>`;
+      });
+      const body = dogList.replace(/#{dogsList}/g, dogItems);
+      res.statusCode = 200;
+      res.setHeader('content-type', 'text/html');
+
+      res.write(body)
+      res.end();
+      return;
     }
 
     // Phase 2: GET /dogs/new
     if (req.method === 'GET' && req.url === '/dogs/new') {
-      // Your code here 
+      const createPage = fs.readFileSync('./views/create-dog.html', 'utf-8')
+      res.statusCode = 200;
+      res.setHeader('content-type', 'text/html');
+      res.write(createPage);
+      res.end();
+      return;
+
     }
 
     // Phase 3: GET /dogs/:dogId
     if (req.method === 'GET' && req.url.startsWith('/dogs/')) {
+      const dogDetails = fs.readFileSync('./views/dog-details.html', 'utf-8');
+
       const urlParts = req.url.split('/');
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
         const dog = dogs.find(dog => dog.dogId === Number(dogId));
-        // Your code here 
+        if (dog) {
+
+          const details = dogDetails.replace(/#{name}/g, dog.name).replace(/#{age}/g, dog.age);
+
+          res.statusCode = 200;
+          res.setHeader('content-type', 'text/html');
+          res.write(details);
+          res.end();
+          return;
+
+        } else {
+
+          const htmlPage = fs.readFileSync("./views/error.html", 'utf-8');
+          const resBody = htmlPage
+            .replace(/#{message}/g, 'Dog Not Found');
+
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          res.write(resBody);
+          return res.end();
+
+        }
       }
     }
 
     // Phase 4: POST /dogs
     if (req.method === 'POST' && req.url === '/dogs') {
       // Your code here 
+      let name = req.body.name;
+      let age = req.body.age;
+      let id = getNewDogId();
+      dogs.push({ dogId: id, name, age });
+      res.statusCode = 302;
+      res.setHeader('location', `/dogs/${id}`);
+      res.end();
+      return;
     }
 
     // Phase 5: GET /dogs/:dogId/edit
@@ -117,7 +167,21 @@ const server = http.createServer((req, res) => {
       if (urlParts.length === 4 && urlParts[3] === 'edit') {
         const dogId = urlParts[2];
         const dog = dogs.find(dog => dog.dogId === Number(dogId));
-        // Your code here 
+        if (!dog) {
+          const htmlPage = fs.readFileSync("./views/error.html", 'utf-8');
+          const resBody = htmlPage
+            .replace(/#{message}/g, 'Dog Not Found');
+
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          res.write(resBody);
+          return res.end();
+        }
+        let url = fs.readFileSync(`./views/edit-dog.html`, 'utf-8').replace(/#{dogId}/g, dogId).replace(/#{name}/g, dog.name).replace(/#{age}/g, dog.age);
+        res.setHeader('content-type', 'text/html');
+        res.write(url);
+        res.end();
+        return;
       }
     }
 
@@ -127,7 +191,22 @@ const server = http.createServer((req, res) => {
       if (urlParts.length === 3) {
         const dogId = urlParts[2];
         const dog = dogs.find(dog => dog.dogId === Number(dogId));
-        // Your code here 
+        if (!dog) {
+          const htmlPage = fs.readFileSync("./views/error.html", 'utf-8');
+          const resBody = htmlPage
+            .replace(/#{message}/g, 'Dog Not Found');
+
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html");
+          res.write(resBody);
+          return res.end();
+        }
+        dog.age = req.body.age;
+        dog.name = req.body.name;
+        res.statusCode = 302;
+        res.setHeader('location', `/dogs/${dogId}`);
+        res.end();
+        return;
       }
     }
 
